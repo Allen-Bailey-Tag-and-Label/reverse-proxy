7adc3aaf639dcd729aac3a09aae264a855517208
bobthered <bobmcaleavey@gmail.com>
2025-07-21 14:12:38 -0400

    feat: check other info

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -3,26 +3,26 @@ import { PROXY_SECRET } from "$env/static/private";
 
 export const POST: RequestHandler = async (event) => {
     const { request } = event;
-    const { headers } = request;
     const secret = request.headers.get('x-proxy-secret')
 
     if (!secret || secret !== PROXY_SECRET) return json({ error: 'Unauthorized' }, { status: 403 });
 
-    // let bodyJSON;
-    // try {
-    //     bodyJSON = await request.json();
-    // } catch {
-    //     return json({ error: `Invalid JSON` }, { status: 400 })
-    // }
+    let bodyJSON;
+    try {
+        bodyJSON = await request.json();
+    } catch {
+        return json({ error: `Invalid JSON` }, { status: 400 })
+    }
 
-    // const { url, method = "POST", headers = {}, body } = bodyJSON;
+    const { url, method = "POST", headers = {}, body } = bodyJSON;
 
-    // if (!url || typeof url !== 'string' || !url.startsWith('https://')) return json({ error: 'Invalid or missing url' }, { status: 400 });
+    if (!url || typeof url !== 'string' || !url.startsWith('https://')) return json({ error: 'Invalid or missing url' }, { status: 400 });
+
+    return json({ url, method, headers, body })
 
     // const response = await fetch(url, { method, headers, body });
     // const result = await response.json();
 
     // return result;
 
-    return json({ event: JSON.stringify(event, null, 2), headers, secret })
 }
