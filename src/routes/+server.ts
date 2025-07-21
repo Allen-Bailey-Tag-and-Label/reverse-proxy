196970c6af46888c75b320b694aad988a02d3246
bobthered <bobmcaleavey@gmail.com>
2025-07-21 13:18:15 -0400

    feat: initialize server endpoint

---
+++ src/routes/+server.ts
@@ -0,0 +1,23 @@
+import { json, type RequestHandler } from "@sveltejs/kit";
+import { PROXY_SECRET } from '$env/static/private'
+
+export const POST: RequestHandler = async ({ request }) => {
+    const secret = request.headers.get('x-proxy-secret');
+    if (!secret || secret !== PROXY_SECRET) return json({ error: 'Unauthorized' }, { status: 403 });
+
+    let bodyJSON;
+    try {
+        bodyJSON = await request.json();
+    } catch {
+        return json({ error: `Invalid JSON` }, { status: 400 })
+    }
+
+    const { url, method = "POST", headers = {}, body } = bodyJSON;
+
+    if (!url || typeof url !== 'string' || !url.startsWith('https://')) return json({ error: 'Invalid or missing url' }, { status: 400 });
+
+    const response = await fetch(url, { method, headers, body });
+    const result = await response.json();
+
+    return result;
+}
